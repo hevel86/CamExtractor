036f77fd4291204ad7a4fea0773d2e5b97aebc66
Michael <johndoe86x@gmail.com>
2022-05-31 14:32:21 -0400

    Added pretty tables package and generated a single column table for the campoints Added docstrings to functions Generated new test.csv file

--- main.py
+++ main.py
@@ -7,23 +7,28 @@ import openpyxl
 import os
 import sys
 from tkinter.filedialog import *
+from prettytable import PrettyTable
 
 # Declare variables
 CSV_HEADER = '% MA_PERIODE=1 SL_PERIODE=1 CYCLIC=1'
 DEGREES_COLUMN = 'DEGREES'
-export_filename = "test.csv"
+EXPORT_FILENAME = "test.csv"
 
 def openFile():
+    """Choose an excel file to extract the campoints from"""
     print("Open file")
     filename = askopenfilename(filetypes=[("Excel files",".xlsx .xls")])
-    print(filename)
+    print(f"Filename: {filename}")
     return filename
 
 def pandaManipulation(excel_file):
+    """Use the pandas library to retrieve the degrees column and create a list"""
     # Read excel file in pandas
     df = pd.read_excel(filename)
     # Extract raw campoints from the DEGREES column
     raw_campoints = df[DEGREES_COLUMN].tolist()
+
+
     #Declare empty list
     campoints_list = []
     # Divide by 360 for regular cam
@@ -32,14 +37,15 @@ def pandaManipulation(excel_file):
 
     # Replace the last value with 0
     campoints_list[len(campoints_list) - 1] = 0
-    print(campoints_list)
+    # print(campoints_list)
     return campoints_list
 
 def exportCSV(csv_campoints):
+    """Use the pandas library to export the campoints to CSV"""
     print("Export CSV")
     # Pandas dataframe to export CSV
     df = pd.DataFrame(csv_campoints, columns=[CSV_HEADER])
-    df.to_csv(export_filename, index=False)
+    df.to_csv(EXPORT_FILENAME, index=False)
 
 # Call functions
 try:
@@ -49,6 +55,12 @@ try:
     br_campoints = pandaManipulation(filename)
     # Export CSV using pandas to_csv functionality
     exportCSV(br_campoints)
+
+    # create a campoints table to print to the console
+    table = PrettyTable()
+    table.add_column("Cam Points", br_campoints)
+    table.align = "l"
+    print(table)
 except:
     print("Exception Error")
 
