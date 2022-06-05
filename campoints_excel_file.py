903acc11559c8ccc29379c32aa1fe08af85f6b1c
Michael Dages <johndoe86x@gmail.com>
2022-06-05 18:30:23 -0400

    Added docstrings Added csv export attribute to campoints_excel_file.py

--- campoints_excel_file.py
+++ campoints_excel_file.py
@@ -2,9 +2,10 @@ from tkinter.filedialog import *
 import os
 
 class CampointsExcelFile:
+    """Retrieve excel file and store csv filename in the same path"""
     def __init__(self):
         self.filename_with_path = askopenfilename(filetypes=[("Excel files", ".xlsx .xls")])
-        self.filename_without_extension = os.path.splitext(self.filename_with_path)[0]
-        self.filename = os.path.basename(self.filename_with_path)
-        self.filepath = os.path.dirname(self.filename_with_path)
-
+        self.___filename_without_extension = os.path.splitext(self.filename_with_path)[0]
+        self.csv_export_filename = self.___filename_without_extension + ".csv"
+        self.___filename = os.path.basename(self.filename_with_path)
+        self.___filepath = os.path.dirname(self.filename_with_path)
