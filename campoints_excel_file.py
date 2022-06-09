7c21156a80e96ffbbb81bf9d6856ef0c5dbc1242
Michael Dages <michael.dages@mbd-inc.com>
2022-06-09 15:53:18 -0400

    Exposed filename to use in graph title

--- campoints_excel_file.py
+++ campoints_excel_file.py
@@ -14,7 +14,7 @@ class CampointsExcelFile:
         self.filename_with_path = askopenfilename(filetypes=[("Excel files", ".xlsx .xls")])
         self.___filename_without_extension = os.path.splitext(self.filename_with_path)[0]
         self.csv_export_filename = self.___filename_without_extension + ".csv"
-        self.___filename = os.path.basename(self.filename_with_path)
+        self.base_filename = os.path.basename(self.filename_with_path)
         self.___filepath = os.path.dirname(self.filename_with_path)
         self.dataframe = None
         self.is_rotodex_cam = False
