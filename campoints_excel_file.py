504ca57d860ce7d1b854afe7992820e6e9d35fca
Michael Dages <michael.dages@mbd-inc.com>
2022-06-16 11:36:25 -0400

    Added comments in declaration method Added filename retrieval without filepath or file extension

--- campoints_excel_file.py
+++ campoints_excel_file.py
@@ -11,9 +11,15 @@ class CampointsExcelFile:
     """Retrieve Excel file and store csv filename in the same path"""
 
     def __init__(self):
+        # Retrieve filename + UNC
         self.filename_with_path = askopenfilename(filetypes=[("Excel files", ".xlsx .xls")])
+        # Get the full UNC without the extension
         self.filename_without_extension = os.path.splitext(self.filename_with_path)[0]
+        # Filename only, no extension, no path
+        self.figure_title = os.path.basename(self.filename_with_path.split('.')[0])
+        # Full UNC with csv extension
         self.csv_export_filename = self.filename_without_extension + ".csv"
+        # Filename only with extension
         self.base_filename = os.path.basename(self.filename_with_path)
         self.___filepath = os.path.dirname(self.filename_with_path)
         self.dataframe = None
